4c196245d325704daab8a6684fa6a0f21e7a837d
AdamSimpson <adambsimpson@gmail.com>
2017-06-05 13:46:50 -0400

    Modify container primary through environment variables

--- Titan/TitanPrep.sh
+++ Titan/TitanPrep.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
 
-### Create directories and symlinks between /opt/cray/nvidia/lib64/* and /usr/lib
-# sudo singularity exec -w ./TitanPrep.sh
+####
+## Append to #post in bootstrap or run after container creation
+## sudo singularity exec -w ./TitanPrep.sh
+####
 
 # Print commands executed
 set -x
-
-# Check to see if the singularity modulefile is loaded
-echo '[ -z "$SINGULARITY_MODULE_LOADED" ] && echo "WARNING: singularity module not load!"' >> /environment
-
-# Don't pass PYTHONSTARTUP into container
-echo "unset PYTHONSTARTUP" >> /environment
-# Don't pass PKG_CONFIG_PATH unless requested
-echo '[ -z "$KEEP_PKG_CONFIG" ] && unset PKG_CONFIG_PATH' >> /environment
-
-####
-# Setup Cray MPI
-# These variables are expected to be set at container runtime(e.g. in the module file loading the container)
-# SYSUTILS_DEFAULT_DIR=`readlink -f /opt/cray/sysutils/default`
-# WLM_DEFAULT_DIR=`readlink -f /opt/cray/wlm_detect/default`
-# CRAY_NVIDIA_DRIVER_LIB_DIR=`readlink -f /opt/cray/nvidia/default/lib64`
-####
-
-# Make sure Cray MPI/ALPS related libraries are in container LD_LIBRARY_PATH
-echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CRAY_LD_LIBRARY_PATH}:${SYSUTILS_DEFAULT_DIR}/lib64:${WLM_DEFAULT_DIR}/lib64' >> /environment
-
-####
-# Setup Cray-NVIDIA driver lib/bins
-# These variables are expected to be set at container runtime(e.g. in the module file loading the container)
-# CRAY_NVIDIA_DRIVER_LIB_DIR=`readlink -f /opt/cray/nvidia/default/lib64`
-# CRAY_NVIDIA_DRIVER_BIN_DIR=`readlink -f /opt/cray/nvidia/default/bin`
-####
-echo 'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CRAY_NVIDIA_DRIVER_LIB_DIR}' >> /environment
-echo 'export PATH=$PATH:${CRAY_NVIDIA_DRIVER_BIN_DIR}' >> /environment
 
 # Mount point for Cray files
 mkdir -p /opt/cray
@@ -45,31 +19,3 @@ mkdir -p /var/opt/cray
 mkdir -p /lustre/atlas
 mkdir -p /lustre/atlas1
 mkdir -p /lustre/atlas2
-
-### Create symlinks between libmpi* and Cray's mpich
-c_mpich=`ldconfig -p | grep libmpich.so | awk 'NR==1 {print $4}' | xargs readlink -f`
-cxx_mpich=`ldconfig -p | grep libmpichcxx.so | awk 'NR==1 {print $4}' | xargs readlink -f`
-f_mpich=`ldconfig -p | grep libmpichfort.so | awk 'NR1==1 {print $4}' | xargs readlink -f`
-
-if [ ! -f ${c_mpich} ]; then
-  echo "libmpich.so not found!"
-  exit 1
-fi
-if [ ! -f ${cxx_mpich} ]; then
-  echo "libmpichcxx.so not found!"
-  exit 1
-fi
-if [ ! -f ${f_mpich} ]; then
-  echo "libmpichfort.so not found!"
-  exit 1
-fi
-
-# Keep a copy of original
-mv ${c_mpich} ${c_mpich}.original
-mv ${cxx_mpich} ${cxx_mpich}.original
-mv ${f_mpich} ${f_mpich}.original
-
-# Replace MPICH libraries with symlink to Cray MPT libraries
-ln -nsf /sw/titan/singularity/mpich/lib/libmpich.so ${c_mpich}
-ln -nsf /sw/titan/singularity/mpich/lib/libmpichcxx.so ${cxx_mpich}
-ln -nsf /sw/titan/singularity/mpich/lib/libfmpich.so ${f_mpich}
