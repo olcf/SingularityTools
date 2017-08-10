51974a63677ba79f7d8c71d239b04bf11f3e5176
AdamSimpson <adambsimpson@gmail.com>
2017-08-10 18:13:57 -0400

    Add directory creator...which doesn't work on Titan

---
+++ Titan/patch_singularity_titan.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+image=../$1
+declare -a directories=("opt/cray"
+                        "var/spool/alps"
+                        "var/opt/cray"
+                        "lustre/atlas"
+                        "lustre/atlas1"
+                        "lustre/atlas2"
+                        "sw"
+                        "ccs/sw"
+                        "autofs/nccs-svm1_sw")
+
+mkdir -p .singularity-fixer && cd .singularity-fixer
+
+for dir in "${directories[@]}"; do
+  mkdir -p $dir
+  singularity copy $image --parents -r $dir /
+done
+
+cd ..
+rm -rf .singularity-fixer
