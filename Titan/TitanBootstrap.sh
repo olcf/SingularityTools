f8be07a0796d33bfff7f5b126b6f67a00f6245dd
AdamSimpson <adambsimpson@gmail.com>
2017-08-11 09:07:15 -0400

    re-add bootstrap prep script

---
+++ Titan/TitanBootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+####
+## Append to #post in bootstrap or run after container creation
+## sudo singularity exec -w ./TitanPrep.sh
+####
+
+# Print commands executed
+set -x
+
+# Mount point for Cray files
+mkdir -p /opt/cray
+
+# Mount point for Cray files needed for ALSP runtime
+mkdir -p /var/spool/alps
+mkdir -p /var/opt/cray
+
+# Mount point for lustre
+mkdir -p /lustre/atlas
+mkdir -p /lustre/atlas1
+mkdir -p /lustre/atlas2
+
+# Mount point for /sw
+mkdir -p /sw
+mkdir -p /ccs/sw
+mkdir -p /autofs/nccs-svm1_sw
