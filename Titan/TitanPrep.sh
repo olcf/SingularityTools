589f0ef717e532371184fadec9a841f1cc887741
AdamSimpson <adambsimpson@gmail.com>
2017-08-11 09:05:18 -0400

    Rename prep script

--- Titan/TitanPrep.sh
+++ Titan/TitanPrep.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-####
-## Append to #post in bootstrap or run after container creation
-## sudo singularity exec -w ./TitanPrep.sh
-####
+image=../$1
+declare -a directories=("opt/cray"
+                        "var/spool/alps"
+                        "var/opt/cray"
+                        "lustre/atlas"
+                        "lustre/atlas1"
+                        "lustre/atlas2"
+                        "sw"
+                        "ccs/sw"
+                        "autofs/nccs-svm1_sw")
 
-# Print commands executed
-set -x
+mkdir -p .singularity-fixer && cd .singularity-fixer
 
-# Mount point for Cray files
-mkdir -p /opt/cray
+for dir in "${directories[@]}"; do
+  mkdir -p $dir
+  tar -cp $dir | singularity import $image
+done
 
-# Mount point for Cray files needed for ALSP runtime
-mkdir -p /var/spool/alps
-mkdir -p /var/opt/cray
-
-# Mount point for lustre
-mkdir -p /lustre/atlas
-mkdir -p /lustre/atlas1
-mkdir -p /lustre/atlas2
-
-# Mount point for /sw
-mkdir -p /sw
-mkdir -p /ccs/sw
-mkdir -p /autofs/nccs-svm1_sw
+cd .. && rm -rf .singularity-fixer
