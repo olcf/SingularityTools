1f1cb1bd5def02a0ddfda088e2ed87b43259232a
AdamSimpson <adambsimpson@gmail.com>
2017-09-30 13:56:04 -0400

    Make catch tests compile

--- Builder/Server/SSH_Sanitizer/tests.cpp
+++ Builder/Server/SSH_Sanitizer/tests.cpp
@@ -1,9 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 SCENARIO("SSH_Sanitizer only allows valid commands") {
   GIVEN("SSH_CONNECTION is set") {
-    WHEN("") {
-      THEN("") {
+    WHEN("some stuff happens") {
+      THEN("bloop") {
       }
     }
   }
