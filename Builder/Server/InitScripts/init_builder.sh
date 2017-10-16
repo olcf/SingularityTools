5c52ae7a8df250f76cc40beebf6577995860e803
AdamSimpson <adambsimpson@gmail.com>
2017-10-16 11:30:27 -0400

    fix quotes

--- Builder/Server/InitScripts/init_builder.sh
+++ Builder/Server/InitScripts/init_builder.sh
@@ -26,12 +26,12 @@ vagrant box add ~/VagrantBase/singularity_builder.box --name SingularityBuilder
 # build_queue contains a list of queued builds
 # active_builds contains a single row and colum containing the number of active builds
 # We use AUTOINCREMENT so we can can sort our queue
-sqlite3 /home/builder/BuildQueue.db 'CREATE TABLE queue(id INTEGER PRIMARY KEY, status TEXT, timestamp INTEGER DEFAULT (strftime('%s','now')));'
-sqlite3 /home/builder/ResourceManager.db 'CREATE TABLE slot(id INTEGER PRIMARY KEY, status TEXT);'
-sqlite3 /home/builder/ResourceManager.db 'INSERT INTO slot(id, status) VALUES(1, 0);'
-sqlite3 /home/builder/ResourceManager.db 'INSERT INTO slot(id, status) VALUES(2, 0);'
-sqlite3 /home/builder/ResourceManager.db 'INSERT INTO slot(id, status) VALUES(3, 0);'
-sqlite3 /home/builder/ResourceManager.db 'INSERT INTO slot(id, status) VALUES(4, 0);'
+sqlite3 /home/builder/BuildQueue.db "CREATE TABLE queue(id INTEGER PRIMARY KEY, status TEXT, timestamp INTEGER DEFAULT (strftime('%s','now')));"
+sqlite3 /home/builder/ResourceManager.db "CREATE TABLE slot(id INTEGER PRIMARY KEY, status TEXT);"
+sqlite3 /home/builder/ResourceManager.db "INSERT INTO slot(id, status) VALUES(1, 0);"
+sqlite3 /home/builder/ResourceManager.db "INSERT INTO slot(id, status) VALUES(2, 0);"
+sqlite3 /home/builder/ResourceManager.db "INSERT INTO slot(id, status) VALUES(3, 0);"
+sqlite3 /home/builder/ResourceManager.db "INSERT INTO slot(id, status) VALUES(4, 0);"
 
 # Create builder scratch work directory
 mkdir /home/builder/container_scratch
