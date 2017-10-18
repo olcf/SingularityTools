348f150df8d7084cebb4ec05f9397c5f7e49a5fa
atj <adambsimpson@gmail.com>
2017-10-18 14:40:31 -0400

    Add resource_manager tests

--- Builder/Server/SafeSH/tests/resource_manager.cpp
+++ Builder/Server/SafeSH/tests/resource_manager.cpp
@@ -21,21 +21,43 @@ TEST_CASE("Slots can be reserved and released") {
     TMP_DB db;
 
     SECTION("All the available slots can be reserved but no more") {
+        std::unique_ptr<builder::ResourceManager> resources_a;
+        std::unique_ptr<builder::ResourceManager> resources_b;
+        std::unique_ptr<builder::ResourceManager> resources_c;
+
+        // Initialize resource managers
         std::thread a([&]() {
-            builder::ResourceManager resources;
-            REQUIRE(resources.reserve_slot() == true);
+            resources_a = std::make_unique<builder::ResourceManager>();
         });
         std::thread b([&]() {
-            builder::ResourceManager resources;
-            REQUIRE(resources.reserve_slot() == true);
+            resources_b = std::make_unique<builder::ResourceManager>();
+
         });
         std::thread c([&]() {
-            builder::ResourceManager resources;
-            REQUIRE(resources.reserve_slot() == true);
+            resources_c = std::make_unique<builder::ResourceManager>();
         });
-
         a.join();
         b.join();
         c.join();
+
+        // Reserve slots in parallel from each resource manager instance
+        // Trying to reserve more than available slots will return false
+        std::thread d([&]() {
+            REQUIRE(resources_a->reserve_slot() == true);
+        });
+        std::thread e([&]() {
+            REQUIRE(resources_b->reserve_slot() == true);
+        });
+        std::thread f([&]() {
+            REQUIRE(resources_c->reserve_slot() == false);
+        });
+        d.join();
+        e.join();
+        f.join();
+    }
+
+    SECTION("Resources should be free'd one resource deconstruction") {
+        builder::ResourceManager resources;
+        REQUIRE(resources.reserve_slot() == true);
     }
 }
