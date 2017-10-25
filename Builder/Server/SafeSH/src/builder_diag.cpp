4eb853370c61b50a43e0071cb819ebbefd786e8b
atj <adambsimpson@gmail.com>
2017-10-25 16:27:21 -0400

    Typo in print statements

--- Builder/Server/SafeSH/src/builder_diag.cpp
+++ Builder/Server/SafeSH/src/builder_diag.cpp
@@ -10,10 +10,10 @@ int main(int argc, char **argv) {
     std::cout << "Resources free: "<< ResourceManager::get_count(ResourceManager::SlotStatus::free)<<std::endl;
     std::cout << "Resources reserved: "<< ResourceManager::get_count(ResourceManager::SlotStatus::reserved)<<std::endl;
 
-    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::running)<<std::endl;
-    std::cout << "Resources queued: "<< BuildQueue::get_count(BuildQueue::JobStatus ::queued)<<std::endl;
-    std::cout << "Resources finished: "<< BuildQueue::get_count(BuildQueue::JobStatus ::finished)<<std::endl;
-    std::cout << "Resources killed: "<< BuildQueue::get_count(BuildQueue::JobStatus ::killed)<<std::endl;
+    std::cout << "Builds running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::running)<<std::endl;
+    std::cout << "Builds queued: "<< BuildQueue::get_count(BuildQueue::JobStatus ::queued)<<std::endl;
+    std::cout << "Builds finished: "<< BuildQueue::get_count(BuildQueue::JobStatus ::finished)<<std::endl;
+    std::cout << "Builds killed: "<< BuildQueue::get_count(BuildQueue::JobStatus ::killed)<<std::endl;
 
     return 0;
 }
