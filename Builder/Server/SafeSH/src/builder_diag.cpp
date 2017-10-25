3212ed0ffc9e137fab0a1663381865b896359120
atj <adambsimpson@gmail.com>
2017-10-25 16:22:35 -0400

    Fix print formatting

--- Builder/Server/SafeSH/src/builder_diag.cpp
+++ Builder/Server/SafeSH/src/builder_diag.cpp
@@ -7,13 +7,13 @@ using namespace builder;
 
 // Print builder diagnostic information
 int main(int argc, char **argv) {
-    std::cout << "Resources free: "<< ResourceManager::get_count(ResourceManager::SlotStatus::free);
-    std::cout << "Resources reserved: "<< ResourceManager::get_count(ResourceManager::SlotStatus::reserved);
+    std::cout << "Resources free: "<< ResourceManager::get_count(ResourceManager::SlotStatus::free)<<std::endl;
+    std::cout << "Resources reserved: "<< ResourceManager::get_count(ResourceManager::SlotStatus::reserved)<<std::endl;
 
-    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::running);
-    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::queued);
-    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::finished);
-    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::killed);
+    std::cout << "Resources running: "<< BuildQueue::get_count(BuildQueue::JobStatus ::running)<<std::endl;
+    std::cout << "Resources queued: "<< BuildQueue::get_count(BuildQueue::JobStatus ::queued)<<std::endl;
+    std::cout << "Resources finished: "<< BuildQueue::get_count(BuildQueue::JobStatus ::finished)<<std::endl;
+    std::cout << "Resources killed: "<< BuildQueue::get_count(BuildQueue::JobStatus ::killed)<<std::endl;
 
     return 0;
 }
